7cc1ea1b0584d98a49bff8d79cf184a447049026
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-27 17:31:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function BoardMember(name, homeState, training){
     return 'No, I must disagree'
   }
   
-  User.prototype.approve = function(){
+  .prototype.approve = function(){
     return 'You can do that!'
   }
   
